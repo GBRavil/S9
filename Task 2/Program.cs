df53a490c08a7cdc009d43462d7801d1d3cf1ee5
Ravil <lllx2@yandex.ru>
2022-11-14 14:53:22 +0300

    2

---
+++ Task 2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+Console.WriteLine($"Задайте количество строк (M)");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Задайте количество столбцов (N)");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for(int i = m; i<=n; i++){
+    sum +=i;
+}
+Console.Write($"Сумма элементов от {m} до {n} = {sum}");
