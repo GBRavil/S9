d722f538e5f48d37a43c267118abaf079d44e957
Ravil <lllx2@yandex.ru>
2022-11-14 17:37:01 +0300

    2

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
-Console.WriteLine($"Задайте количество строк (M)");
+Console.WriteLine($"Задайте (M)");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Задайте количество столбцов (N)");
+Console.WriteLine($"Задайте (N)");
 int n = Convert.ToInt32(Console.ReadLine());
 if(m<n){
     for(int i = m; i<=n; i++){
--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,10 +1,14 @@
 ﻿// Найти сумму элементов от M до N, N и M заданы
-Console.WriteLine($"Задайте количество строк (M)");
+Console.WriteLine($"Задайте (M)");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Задайте количество столбцов (N)");
+Console.WriteLine($"Задайте (N)");
 int n = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-for(int i = m; i<=n; i++){
-    sum +=i;
-}
-Console.Write($"Сумма элементов от {m} до {n} = {sum}");
+Console.WriteLine($"Result = {SumNumbers(m, n)}");
+
+int SumNumbers(int m, int n) {
+    if (m == 0) return (n * (n + 1)) / 2;            
+    else if (n == 0) return (m * (m - 1)) / 2 * -1;  
+    else if (m == n) return m;                       
+    else if (m < n) return n + SumNumbers(m, n-1);
+    else return n + SumNumbers(n+1, m);            
+}    
