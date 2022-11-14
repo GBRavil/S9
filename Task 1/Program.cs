b84359bff68b2d6f64460aedd1b7fa84a1e2f8de
Ravil <lllx2@yandex.ru>
2022-11-14 14:44:27 +0300

    1

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine($"Задайте количество строк (M)");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Задайте количество столбцов (N)");
+int n = Convert.ToInt32(Console.ReadLine());
+if(m<n){
+    for(int i = m; i<=n; i++){
+        Console.Write($"{i} ");
+    }
+} else {
+    for(int i = n; i<=m; i++){
+        Console.Write($"{i} ");
+    }
+}
